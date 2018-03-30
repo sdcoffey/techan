7c6aed38874868ce73fe2bea684b65b01fc1afe2
sdcoffey <sdcoffey@umich.edu>
2018-03-30 10:46:52 -0700

    Update CrossIndicator

--- indicator_cross.go
+++ indicator_cross.go
@@ -3,30 +3,41 @@ package talib4g
 import "github.com/sdcoffey/big"
 
 type crossIndicator struct {
-	differenceIndicator
+	upper Indicator
+	lower Indicator
 }
 
 // Returns a new CrossIndicator, which, given an index, determines whether a lower
 // indicator has crossed an upper one
 func NewCrossIndicator(upper, lower Indicator) Indicator {
 	return crossIndicator{
-		differenceIndicator{
-			minuend:    upper,
-			subtrahend: lower,
-		},
+		upper: upper,
+		lower: lower,
 	}
 }
 
 // Walks backward from the current index to determine if the lower indicator
 // has crossed the upper indicator. Returns truthy value if so, zero otherwise
 func (ci crossIndicator) Calculate(index int) big.Decimal {
-	if index == 0 {
+	i := index
+
+	if i == 0 || ci.upper.Calculate(i).GTE(ci.lower.Calculate(i)) {
 		return big.ZERO
 	}
 
-	if ci.differenceIndicator.Calculate(index).LTE(big.ZERO) && ci.differenceIndicator.Calculate(index-1).GT(big.ZERO) {
-		return big.ONE
-	}
+	i--
 
-	return big.ZERO
+	if ci.upper.Calculate(i).GT(ci.lower.Calculate(i)) {
+		return big.ONE
+	} else {
+		for i > 0 && ci.upper.Calculate(i).EQ(ci.lower.Calculate(i)) {
+			i--
+		}
+
+		if i != 0 && ci.upper.Calculate(i).GT(ci.lower.Calculate(i)) {
+			return big.ONE
+		}
+
+		return big.ZERO
+	}
 }
