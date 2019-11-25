ff21c36a07e0546764126acfe89b6d31ae9d59ed
sdcoffey <stevendcoffey@gmail.com>
2019-11-25 09:51:49 -0500

    add trendline indicator

---
+++ indicator_trend.go
@@ -0,0 +1,70 @@
+package techan
+
+import "github.com/sdcoffey/big"
+
+type trendLineIndicator struct {
+	indicator Indicator
+	window    int
+}
+
+// NewTrendlineIndicator returns an indicator whose output is the slope of the trend
+// line given by the values in the window.
+func NewTrendlineIndicator(indicator Indicator, window int) Indicator {
+	return trendLineIndicator{
+		indicator: indicator,
+		window:    window,
+	}
+}
+
+func (tli trendLineIndicator) Calculate(index int) big.Decimal {
+	values := make([]big.Decimal, tli.window)
+
+	for i := 0; i < tli.window; i++ {
+		values[i] = tli.indicator.Calculate(index - (tli.window - 1) + i)
+	}
+
+	n := big.ONE.Mul(big.NewDecimal(float64(tli.window)))
+	ab := sumXy(values).Mul(n).Sub(sumX(values).Mul(sumY(values)))
+	cd := sumX2(values).Mul(n).Sub(sumX(values).Pow(2))
+
+	return ab.Div(cd)
+}
+
+func sumX(decimals []big.Decimal) (s big.Decimal) {
+	s = big.ZERO
+
+	for i := range decimals {
+		s = s.Add(big.NewDecimal(float64(i)))
+	}
+
+	return s
+}
+
+func sumY(decimals []big.Decimal) (b big.Decimal) {
+	b = big.ZERO
+	for _, d := range decimals {
+		b = b.Add(d)
+	}
+
+	return
+}
+
+func sumXy(decimals []big.Decimal) (b big.Decimal) {
+	b = big.ZERO
+
+	for i, d := range decimals {
+		b = b.Add(d.Mul(big.NewDecimal(float64(i))))
+	}
+
+	return
+}
+
+func sumX2(decimals []big.Decimal) big.Decimal {
+	b := big.ZERO
+
+	for i := range decimals {
+		b = b.Add(big.NewDecimal(float64(i)).Pow(2))
+	}
+
+	return b
+}
---
+++ indicator_trend_test.go
@@ -0,0 +1,42 @@
+package techan
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrendIndicator(t *testing.T) {
+	t.Run("returns the correct slope of the trend", func(t *testing.T) {
+		tests := []struct {
+			series         []float64
+			expectedResult string
+		}{
+			{
+				series:         []float64{0, 1, 2, 3},
+				expectedResult: "1",
+			},
+			{
+				series:         []float64{0, 2, 4, 6},
+				expectedResult: "2",
+			},
+			{
+				series:         []float64{5, 4, 3, 2},
+				expectedResult: "-1",
+			},
+		}
+
+		for _, test := range tests {
+			series := mockTimeSeriesFl(test.series...)
+			indicator := NewTrendlineIndicator(NewClosePriceIndicator(series), 4)
+
+			assert.EqualValues(t, test.expectedResult, indicator.Calculate(3).String())
+		}
+	})
+
+	t.Run("respects the window", func(t *testing.T) {
+		series := mockTimeSeriesFl(-100, 1000, 0, 1, 2, 3)
+		indicator := NewTrendlineIndicator(NewClosePriceIndicator(series), 4)
+		assert.EqualValues(t, "1", indicator.Calculate(5).String())
+	})
+}
