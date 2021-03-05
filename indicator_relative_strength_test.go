61a09a533bab84a678b57e8fcf8df8e620d7a8c8
Sean Dukehart <tomcruise81@users.noreply.github.com>
2021-03-05 10:34:43 -0500

    Still more unit tests

--- indicator_relative_strength_test.go
+++ indicator_relative_strength_test.go
@@ -38,11 +38,39 @@ func TestRelativeStrengthIndexIndicator(t *testing.T) {
 		decimalEquals(t, 76.2770, indicator.Calculate(23))
 		decimalEquals(t, 77.9908, indicator.Calculate(24))
 		decimalEquals(t, 67.4895, indicator.Calculate(25))
+
+		indicator.RemoveCachedEntry(20)
+
+		decimalEquals(t, 68.4747, indicator.Calculate(15))
+		decimalEquals(t, 64.7836, indicator.Calculate(16))
+		decimalEquals(t, 72.0777, indicator.Calculate(17))
+		decimalEquals(t, 60.7800, indicator.Calculate(18))
+		decimalEquals(t, 63.6439, indicator.Calculate(19))
+		decimalEquals(t, 72.3434, indicator.Calculate(20))
+		decimalEquals(t, 67.3823, indicator.Calculate(21))
+		decimalEquals(t, 68.5438, indicator.Calculate(22))
+		decimalEquals(t, 76.2770, indicator.Calculate(23))
+		decimalEquals(t, 77.9908, indicator.Calculate(24))
+		decimalEquals(t, 67.4895, indicator.Calculate(25))
 	})
 }
 
 func TestRelativeStrengthIndicator(t *testing.T) {
 	indicator := NewRelativeStrengthIndicator(NewClosePriceIndicator(timeseries), 14)
+
+	decimalEquals(t, 2.1721, indicator.Calculate(15))
+	decimalEquals(t, 1.8396, indicator.Calculate(16))
+	decimalEquals(t, 2.5814, indicator.Calculate(17))
+	decimalEquals(t, 1.5497, indicator.Calculate(18))
+	decimalEquals(t, 1.7506, indicator.Calculate(19))
+	decimalEquals(t, 2.6158, indicator.Calculate(20))
+	decimalEquals(t, 2.0658, indicator.Calculate(21))
+	decimalEquals(t, 2.1790, indicator.Calculate(22))
+	decimalEquals(t, 3.2153, indicator.Calculate(23))
+	decimalEquals(t, 3.5436, indicator.Calculate(24))
+	decimalEquals(t, 2.0759, indicator.Calculate(25))
+
+	indicator.RemoveCachedEntry(20)
 
 	decimalEquals(t, 2.1721, indicator.Calculate(15))
 	decimalEquals(t, 1.8396, indicator.Calculate(16))
